6f276b9d884307c7f559d2af802c20f168decf69
jfranco <jfranco@mtrix.com.br>
2024-05-15 22:17:47 -0300

    update(hamburgerButton.addEventListener): scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 const hamburgerButton = document.querySelector('#hamburgerButton');
 const closeButton = document.querySelector('#closeButton');
 const mobileMenu = document.querySelector('#mobileMenu');
+
+hamburgerButton.addEventListener('click', function () {
+  mobileMenu.classList.add('flex');
+});
