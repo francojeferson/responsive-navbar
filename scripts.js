55cf713f075424bb3fbc2be7301e0cea863b9ed2
jfranco <jfranco@mtrix.com.br>
2024-05-15 22:15:00 -0300

    update(closeButton,mobileMenu): scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,3 @@
 const hamburgerButton = document.querySelector('#hamburgerButton');
+const closeButton = document.querySelector('#closeButton');
+const mobileMenu = document.querySelector('#mobileMenu');
